e875611f2d048699519011c14eb51d99d39c6c0d
Axel Biehler <axel.biehler@epitech.eu>
2022-02-24 20:17:02 +0100

    add end parsing

--- src/parsing.py
+++ src/parsing.py
@@ -1,7 +1,3 @@
-from ctypes import alignment
-import filecmp
-from posixpath import split
-
 class Contributor:
     def __init__(self, name, skills) -> None:
         self.name = name
@@ -18,6 +14,11 @@ class Project:
         self.bestBefore = bestBefore
         self.nbRoles = nbRoles
         self.skills = skills
+        self.projects = []
+        self.contributors = []
+    
+    def __str__(self) -> str:
+        return f'name : {self.name}, days : {self.days}, score : {self.score}, best before : {self.nbRoles}, nbRoles : {self.nbRoles}, skills : {self.skills}'
 
 class Parsing:
     def __init__(self, filepath) -> None:
@@ -29,13 +30,14 @@ class Parsing:
         rawValue = f.read().split('\n')
         skills = []
         contributors = []
+        projects = []
 
         nbContributor = int(rawValue[0].split(' ')[0])
         nbProj = int(rawValue[0].split(' ')[1])
 
         tmp = 1
 
-        for i in range(nbContributor):
+        for _ in range(nbContributor):
             skills = []
             name = rawValue[tmp].split(' ')[0]
             nbSkills = int(rawValue[tmp].split(' ')[1])
@@ -51,7 +53,24 @@ class Parsing:
             contributors.append(c)
             skills = []
 
-        print(rawValue[tmp])
+        for _ in range(nbProj):
+            skills = []
+            list = rawValue[tmp].split(' ')
+            nbSkills = int(list[4])
+            tmp += 1
+
+            for _ in range(nbSkills):
+                skillName = rawValue[tmp].split(' ')[0]
+                lvl = rawValue[tmp].split(' ')[1]
+                skills.append((skillName, lvl))
+                tmp += 1
+
+            p = Project(list[0], list[1], list[2], list[3], list[4], skills)
+            projects.append(p)
+            skills = []
+        
+        self.projects = projects
+        self.contributors = contributors
 
 
 
